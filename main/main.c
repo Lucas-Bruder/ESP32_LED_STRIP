e6fa9c2e6d6318c69aaeab8b4a36f3b06d404165
Lucas-Bruder <lucas@mindtribe.com>
2016-11-25 14:46:03 -0600

    Simple cleanup

--- main/main.c
+++ main/main.c
@@ -7,10 +7,7 @@
     ------------------------------------------------------------------------- */
 
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
 #include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "led_strip/led_strip.h"

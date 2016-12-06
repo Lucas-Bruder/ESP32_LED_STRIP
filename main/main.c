a37edd46941c9f48e3686c4d7e61ae21687e62ab
Lucas-Bruder <lucas@mindtribe.com>
2016-12-05 19:40:34 -0800

    Restructed main app

--- main/main.c
+++ main/main.c
@@ -10,52 +10,26 @@
 #include "freertos/task.h"
 
 #include "esp_system.h"
+#include "esp_task.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "led_strip/led_strip.h"
 
 #include <stdio.h>
 
-#define LED_STRIP_LENGTH 17U
-static struct led_color_t led_strip_buf_1[LED_STRIP_LENGTH];
-static struct led_color_t led_strip_buf_2[LED_STRIP_LENGTH];
+extern void main_led_task(void *args);
 
-#define LED_STRIP_RMT_INTR_NUM 19
-
-int app_main(void)
+void app_main(void)
 {
     nvs_flash_init();
-
-    struct led_strip_t led_strip = {
-        .rgb_led_type = RGB_LED_TYPE_WS2812,
-        .rmt_channel = RMT_CHANNEL_1,
-        .rmt_interrupt_num = LED_STRIP_RMT_INTR_NUM,
-        .gpio = GPIO_NUM_21,
-        .led_strip_buf_1 = led_strip_buf_1,
-        .led_strip_buf_2 = led_strip_buf_2,
-        .led_strip_length = LED_STRIP_LENGTH
-    };
-    led_strip.access_semaphore = xSemaphoreCreateBinary();
-
-    bool led_init_ok = led_strip_init(&led_strip);
-    assert(led_init_ok);
-
-    struct led_color_t led_color = {
-        .red = 5,
-        .green = 0,
-        .blue = 0,
-    };
-
-    while (true) {
-        for (uint32_t index = 0; index < LED_STRIP_LENGTH; index++) {
-            led_strip_set_pixel_color(&led_strip, index, &led_color);
-        }
-        led_strip_show(&led_strip);
-
-        led_color.red += 5;
-        vTaskDelay(30 / portTICK_PERIOD_MS);
-    }
-
-    return 0;
+    TaskHandle_t main_task_handle;
+    BaseType_t task_created = xTaskCreate(main_led_task,
+                                            "main_led_task",
+                                            ESP_TASK_MAIN_STACK,
+                                            NULL,
+                                            ESP_TASK_MAIN_PRIO,
+                                            &main_task_handle);
+    (void)task_created;
+    vTaskDelete(NULL);
 }
 
